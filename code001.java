f86614bb4f6169dc9ea328b0e093569d084913bc
Andrew <bandrew235@gmail.com>
2024-09-10 16:21:31 +0300

    Sample program 01
    Personal program created following the notes and other examples. Looking forward to corrections.

---
+++ code001.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// prompt stays on the same line as txt
+public class code001{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Hello, ");
+		System.out.print("World !");
+
+		scanner.close();
+	}
+}
