00ee485c504c969286eabd040ecad416e1075ebe
Andrew <bandrew235@gmail.com>
2024-09-10 16:23:20 +0300

    Sample program 02
    More fundamental code basics.

---
+++ code002.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+// prompt pushed to a new line on print
+public class code002{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Hello, ");
+		System.out.println("World !");
+
+		scanner.close();
+	}
+}
