8d037cafa119f71be550315e56886dec8a8f55f9
Andrew <bandrew235@gmail.com>
2024-09-12 20:31:48 +0300

    Update code001.java

--- code001.java
+++ code001.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 // prompt stays on the same line with the printed text after the program executes successfully.
 public class code001{
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
+		// Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("Hello, ");
 		System.out.print("World !");
 
-		scanner.close();
+		// scanner.close();
 	}
 }
