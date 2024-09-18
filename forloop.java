51b6675a5243b7974fd452a0d60ee30cfac8eb25
Andrew <bandrew235@gmail.com>
2024-09-18 05:15:56 +0300

    "For Loop"
    Iterating through numbers whose maximum is 20, to print out only even numbers.

---
+++ forloop.java
@@ -0,0 +1,11 @@
+// printing all even numbers btwn 1 and 20
+
+public class forloop{
+	public static void main(String[] args){
+		for(int i = 1; i <= 20; i++){
+                   if( i % 2 == 0){
+		       System.out.println("Index: " + i);
+                   }
+		}
+	}
+}
